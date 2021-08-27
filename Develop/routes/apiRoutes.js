b38a7031f361f40c91e8c3423f656eef9986f58d
Janei <18485105+janeijones@users.noreply.github.com>
2021-08-27 06:10:23 -0500

    GET route for workouts set-up

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -3,7 +3,23 @@ const db = require('../models');
 const mongojs = require('mongojs');
 
 
-router.get() //- workouts
+router.get('/workouts', (req, res) => {
+    db.Workout.aggregate([ //aggregate used for values
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: '$exercises.duration'
+          },
+        },
+      },
+    ])
+    .then(Data => { //respond with workout handling
+      res.json(Data);
+    })
+    .catch((err) => {  //error handling
+        res.status(500).json(err) 
+    })
+});
 
 router.post() //- workouts
 
