11cc14155b886fa68fe5a7c81ee0fcc30f9266b3
Janei <18485105+janeijones@users.noreply.github.com>
2021-08-27 05:49:05 -0500

    Added mongoose connect to server.js '

--- Develop/server.js
+++ Develop/server.js
@@ -17,7 +17,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 
 // routes
 
-app.use(require("./routes/ADDROUTESHERE"));
+app.use(require("./routes/apiRoutes"));
+app.use(require('./routes/htmlRoutes'))
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
