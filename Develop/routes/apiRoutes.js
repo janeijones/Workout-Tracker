3dd82b4e177e7197411546ee1a4b0f455ee238fb
Janei <18485105+janeijones@users.noreply.github.com>
2021-08-27 06:23:39 -0500

    Added PUT for updating exercise

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -34,7 +34,29 @@ router.post('/workouts', (req, res) => { //creates workout
 });
 
 
-router.put() //- workouts by id
+router.put("/workouts/:id", (req, res) => { //addds individual workout
+    //const body = req.body
+    //console.log("Body" + body)
+
+db.Workout.findByIdAndUpdate(
+    req.params.id, {
+      $push: {
+        exercises: (req.body)
+      }
+    }, 
+    {
+      runValidators: true
+    }
+  )
+  .then(Data => {
+    res.json(Data); //respond with workout data
+  })
+  .catch((err) => {
+    //error handling
+    res.status(500).json(err)
+  });
+}); 
+
 
 router.get() //- workouts in Range
 
