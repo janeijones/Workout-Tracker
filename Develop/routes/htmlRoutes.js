0eb9b1009e589734cc909490b871c191e9e09fee
Janei <18485105+janeijones@users.noreply.github.com>
2021-08-27 06:37:03 -0500

    Added path join for exercise file, file now able to be accessed

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -4,10 +4,11 @@ const path = require('path')
 router.get('/', (req, res) => {
     //allows use of public index html file
     res.sendFile(path.join(__dirname, '../public/index.html'))
-})
+});
 
 router.get('/exercise', (req, res) => {
-    //sendfile, public exercise html file
+    //allows use of public exercise html file
+    res.sendFile(path.join(__dirname, '../public/exercise.html'))
 })
 
 router.get('/stats', (req, res) => {
