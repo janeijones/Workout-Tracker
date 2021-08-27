588df96fb37d845b66b6ce89a1634a27f5f5ec37
Janei <18485105+janeijones@users.noreply.github.com>
2021-08-27 06:31:30 -0500

    Added routes to accompany html files

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,5 +1,13 @@
 const router = require('express').Router()
 const path = require('path')
 
+router.get('/', (req, res) => {
+    //sendfile, public html file
+})
+
+// router.get(exercise)
+
+// router.get(/stats)
+
 
 module.exports = router;
